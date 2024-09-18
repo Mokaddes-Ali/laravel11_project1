70846ef3cde7950c4e5f2dfcfd25030fcba69d82
mokadde <mokaddes.ru2000@gmail.com>
2024-09-19 00:13:48 +0600

    commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,14 +5,14 @@
 <head>
         <meta charset="utf-8" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        
+
         <title>Dark Sidebar Layout | Shreyu - Responsive Admin Dashboard Template</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href=" {{asset('admin')}}/assets/images/favicon.ico">
+        <link rel="shortcut icon" href="{{asset('admin')}}/assets/images/favicon.ico">
 
         <!-- plugins -->
         <link href="{{asset('admin')}}/assets/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
