d9b4d4215d4a6b1be11cb8d8647c034b22cab947
mokadde <mokaddes.ru2000@gmail.com>
2024-09-28 10:51:01 +0600

    commit

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -84,46 +84,9 @@ public function edit($id){
 
 public function update(Request $request)
 {
-    // dd($request->all());
+    dd($request->all());
 
-    $request->validate([
 
-        'project_id' => 'required',
-        'income_amount' => 'required',
-        'date' => 'required',
-        'note' => 'required',
-        'bank_account_id' => 'required',
-
-    ]);
-
-    $oldincome = Income::where('id',$request->id)->firstOrFail();
-
-    $update=Income::where('id',$request->id)->update([
-        'project_id' => $request->project_id,
-        'income_amount' => $request->income_amount,
-        'date' => $request->date,
-        'note' => $request->note,
-        'bank_account_id' => $request->bank_account_id,
-        'editor' => Auth::user()->id,
-        'slug' => $request->project_id . rand(10000,10000000),
-
-    ]);
-
-    $data=Project::where('id',$request->project_id)->firstOrFail();
-
-    $paid_amount = (float) $request->income_amount + (float) $data->paid_amount - (float)$oldincome->income_amount;
-    $due_amount = (float) $data->project_value - (float)$paid_amount;
-
-    if( $update){
-        $update = Project::where('id',$request->project_id)->update([
-            'paid_amount' => $paid_amount,
-            'due_amount' => $due_amount,
-
-            ]);
-
-     return redirect()->back()->with('success','Data updated successfully');
-
-    }
 }
 
 
