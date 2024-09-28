f39b031f23b2433cc5a1e365d17fc21030a19675
mokadde <mokaddes.ru2000@gmail.com>
2024-09-28 19:26:24 +0600

    commit

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Income;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
     public function index()
     {
-        return view('admin.invoice.index');
+        $invoices = Income::where('project_id', 1)->get();
+        return view('admin.invoice.index', compact('invoices'));
     }
 }
