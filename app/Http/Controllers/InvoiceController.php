10c6acf29b88535769020ab466c4d9c86812ff9c
mokadde <mokaddes.ru2000@gmail.com>
2024-09-28 19:46:43 +0600

    commit

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -11,10 +11,16 @@ class InvoiceController extends Controller
 {
     public function index()
     {
+        // Fetch invoices related to project ID 1
         $invoices = Income::where('project_id', 1)->get();
-        $data = Project::where('project_id', 1)->get();
-        $setting = Settings::where('status', 0) firstOrFail();
 
+        // Fetch the first project with ID 1
+        $data = Project::where('id', 1)->first();  // Use `first()` to get a single project object instead of a collection.
+
+        // Fetch the first setting with status 0
+        $setting = Settings::where('status', 0)->firstOrFail();
+
+        // Return the view with the data compacted into variables
         return view('admin.invoice.index', compact('invoices', 'data', 'setting'));
     }
 }
