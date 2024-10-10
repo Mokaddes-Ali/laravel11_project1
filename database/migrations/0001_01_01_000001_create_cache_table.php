16c46f501cbbe25d24858aa196e4e598010cbc42
mokadde <mokaddes.ru2000@gmail.com>
2024-10-10 23:39:28 +0600

    commit

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
+    /** Run the migrations.
      */
     public function up(): void
     {
