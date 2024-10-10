29553859c762094cd44f8b1b7c27f589e4e22384
mokadde <mokaddes.ru2000@gmail.com>
2024-10-10 08:01:09 +0600

    commit

--- resources/views/admin/Income/show.blade.php
+++ resources/views/admin/Income/show.blade.php
@@ -81,8 +81,8 @@
         <td>
             <a class="btn btn-primary btn-sm," href="{{ url('/income/edit' , $row -> id) }}">edit</a>
             <a class="btn btn-danger btn-sm" onclick="return confirm('Are You Sure Delete!')" href="{{ url('/delete', $row -> id)}}">delete</a>
-            <a class="btn btn-primary btn-sm," href="{{ url('/invoice/create' , $row->paid_amount) }}">invoice</a>
-            <a class="btn btn-secondary btn-sm," href="{{ url('/invoice/pdf' , $row->paid_amount) }}">pdf</a>
+            <a class="btn btn-primary btn-sm," href="{{ url('/invoice/create' , $row ->project_id) }}">invoice</a>
+            <a class="btn btn-secondary btn-sm," href="{{ url('/invoice/pdf' , $row ->project_id) }}">pdf</a>
 
         </td>
       </tr>
